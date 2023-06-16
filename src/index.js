02e2fd5c482c6ce246178e44e705ffde9b9b8ab3
Samuel <samuelou510@gmail.com>
2023-06-15 21:05:27 -0400

    fetch 2 lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
-const init = () => {
-  
-}
+// const init = () => {
+//   const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+//   inputForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const input = document.getElementById("searchByID");
+//     // const input = document.querySelector("input#searchByID");
+//     fetch(`http://localhost:3000/movies/${input.value}`)
+//       .then(res => res.json())
+//       .then(data => {
+//         const title = document.querySelector("section#movieDetails h4");
+//         const summary = document.querySelector("section#movieDetails p");
+
+//         title.innerText = data.title;
+//         summary.innerText = data.summary;
+//       });
+//   });
+// };
+// document.addEventListener('DOMContentLoaded', init);
+// const inputForm = document.querySelector("form");
+
+// inputForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const input = document.getElementById("searchByID");
+
+//   fetch (`http://localhost:3000/movies/${input.value}`)
+//     .then(res => res.json())
+//     .then(data => {
+
+//       const title = document.querySelector("section#movieDetails h4");
+//       const summary = document.querySelector("section#movieDetails p");
+//       title.innerText = data.title;
+//       summary.innerText = data.summary;
+
+//     });
+
+// });
+
+const init = () => {
+  const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
